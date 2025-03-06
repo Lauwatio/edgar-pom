ec6b47b50da8576adf49dcfb62dc1c7c1f349768
Lauwatio <laura.noel307@gmail.com>
2025-03-06 11:04:47 +0100

    style adulte

---
+++ public/adulte.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+$title = "PomKorn";
+$descript = "";
+include_once "../includes/components/head.php";
+?>
+
+<body>
+  <?php include_once "../includes/components/header.php" ?>
+  <main>
+    <h3 class="yellowpom">POM</h3>
+  </main>
+  <?php include_once "../includes/components/footer.php" ?>
+</body>
+
+</html>
