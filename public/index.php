3b48e81f2279b89cf46742b4e03de1d2ae466484
Lauwatio <laura.noel307@gmail.com>
2025-03-06 09:59:51 +0100

    css html home

--- public/index.php
+++ public/index.php
@@ -9,8 +9,10 @@ include_once "../includes/components/head.php";
 <body>
   <?php include_once "../includes/components/header.php" ?>
   <main>
-    <h1>Accueil</h1>
-
+    <span class="yellow">EDGAR<br>POM</span>
+    <h1 class="pink">EDGAR<br>POM</h1>
+    <span class="green">CIDRE<br>et JUS</span>
+    <img src="./assets/images/vache.png" alt="Vache des highlands">
     <div>
       <ul class="questions-wrapper"></ul>
     </div>
