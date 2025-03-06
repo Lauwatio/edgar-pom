4a7c9a86a33be9357ac0613b1ac01c134dc021b8
Lauwatio <laura.noel307@gmail.com>
2025-03-06 11:46:19 +0100

    style PomKorn

--- public/adulte.php
+++ public/adulte.php
@@ -8,8 +8,30 @@ include_once "../includes/components/head.php";
 
 <body>
   <?php include_once "../includes/components/header.php" ?>
+  <h3 class="yellowpom">POM</h3>
   <main>
-    <h3 class="yellowpom">POM</h3>
+    <h1>PomKorn</h1>
+    <p>Faites pétiller votre soirée !</p>
+    <figcaption class="wrap">
+        <figure class="pinkcase">
+            <p>Quizz</p>
+        </figure>
+        <figure class="greencase">
+
+        </figure>
+        <figure class="greencase">
+
+        </figure>
+        <figure class="pinkcase">
+
+        </figure>
+        <figure class="pinkcase">
+
+        </figure>
+        <figure class="greencase">
+
+        </figure>
+</figcaption>
   </main>
   <?php include_once "../includes/components/footer.php" ?>
 </body>
