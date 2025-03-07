b02a9de7917761c2be5c058aefda2626eab5eda0
Lauwatio <laura.noel307@gmail.com>
2025-03-07 15:38:31 +0100

    style quizz_a

--- public/quizz_a.php
+++ public/quizz_a.php
@@ -9,24 +9,33 @@ include_once "../includes/components/head.php";
 <body>
   <?php include_once "../includes/components/header.php" ?>
   <h3 class="yellowpom">POM</h3>
+
   <main class="quizz-main">
     <h1>Quizz PomKorn</h1>
+
+    <figcaption class="joueurs">
+        <h3>Joueurs</h3>
+        <input class="joueur" type="text">
+        <button class="joueur">+</button>
+    </figcaption>
+
     <figcaption class="reponses">
+        <h3>Tour de ...</h3>
         <h2>Enoncé question</h2>
-        <a class="reponse" href="">
+        <button class="reponse">
                 <p>Réponse 1</p>
-        </a>
-        <a class="reponse" href="">
+        </button>
+        <button class="reponse">
                 <p>Réponse 2</p>
-        </a>
-        <a class="reponse" href="">
+        </button>
+        <button class="reponse">
                 <p>Réponse 3</p>
-        </a>
+        </button>
         
         
 </figcaption>
   </main>
-  <?php include_once "../includes/components/footer.php" ?>
+
 </body>
 
 </html>
